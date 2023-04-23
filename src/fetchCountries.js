4a8bd482fb548db615863e20dc05ffc9054a747a
Kamila Adamska <121kama121@gmail.com>
2023-04-23 17:20:03 +0200

    Added debounce

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,11 @@
 const API_URL = 'https://restcountries.com/v3.1';
 
+export const searchParams = new URLSearchParams({
+  fields: 'name,capital,population,flags,languages',
+});
+
 export function fetchCountries(name) {
-  return fetch(`${API_URL}/name/${name}`).then(response => {
+  return fetch(`${API_URL}/name/${name}?${searchParams}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
 const searchBoxEl = document.getElementById('search-box');
 
-searchBoxEl.addEventListener('input', () => {
-  fetchCountries(searchBoxEl.value)
-    .then(users => console.log(users))
-    .catch(error => console.log(error));
-});
+searchBoxEl.addEventListener(
+  'input',
+  debounce(() => {
+    if (!searchBoxEl.value) {
+      return;
+    }
+
+    fetchCountries(searchBoxEl.value.trim())
+      .then(users => console.log(users))
+      .catch(error => console.log(error));
+  }, DEBOUNCE_DELAY)
+);
